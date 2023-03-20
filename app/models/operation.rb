061864dd8236229fc043460deea13ca176039c75
Wassim <wassimchakib@hotmail.com>
2023-03-20 10:55:06 +0000

    Fix rubocop errors

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,5 +2,7 @@ class Operation < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :groups
 
-  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Please enter a valid number' }
+  validates :amount, presence: true,
+                     numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                                     message: 'Please enter a valid number' }
 end
