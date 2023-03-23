9a4ad0e3d6b09b8e86e0482051696709eb26f4a1
Wassim <wassimchakib@hotmail.com>
2023-03-23 00:14:33 +0000

    Add unit test for group

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'wassim', email: 'wassim@gmail.com', password: '123fbc2') }
+  let(:group) { Group.create(name: 'Groceries', icon: 'https://cdn-icons-png.flaticon.com/512/3724/3724720.png', user:) }
+
+  it 'fails because name should be present' do
+    group.name = nil
+    expect(group).to_not be_valid
+  end
+
+  it 'fails because icon should be present' do
+    group.icon = nil
+    expect(group).to_not be_valid
+  end
 end
--- spec/models/operation_spec.rb
+++ spec/models/operation_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Operation, type: :model do
   let(:group) { Group.create(name: 'Groceries', icon: 'https://cdn-icons-png.flaticon.com/512/3724/3724720.png', user:) }
   let(:operation) { Operation.create(name: 'Tomatoes', amount: 5, author: user) }
   it 'fails because name should be present' do
-    user.name = nil
+    operation.name = nil
     expect(operation).to_not be_valid
   end
 
@@ -20,12 +20,11 @@ RSpec.describe Operation, type: :model do
   end
 
   it 'fails because amount should be a number' do
-    operation.amount = "test"
+    operation.amount = 'test'
     expect(operation).to_not be_valid
   end
 
   it 'fails because it should have atleast 1 group' do
     expect(operation).to_not be_valid
   end
-
 end
