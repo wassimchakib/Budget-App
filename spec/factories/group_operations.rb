225ce58d6b73f772464546c0da186b9206586aa6
Wassim <wassimchakib@hotmail.com>
2023-03-23 00:05:51 +0000

    Add unit test for user

--- spec/factories/group_operations.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :group_operation do
-    group { nil }
-    operation { nil }
-  end
-end
--- spec/factories/operations.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :operation do
-    author_id { 1 }
-    name { 'MyString' }
-    amount { 1.5 }
-  end
-end
